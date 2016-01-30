8c3f9f9838f52b332fd16a0e9028a6af20b3a6d3
Josh Sixsmith <josh.sixsmith@gmail.com>
2016-01-30 13:20:08 +1100

    Initial upload of run-parallel.bash

---
+++ examples/run-parallel.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "1 CPU"
+mpiexec -n 1 python parallel_kea_example.py
+
+# if using the KEA library directly or via GDAL, rasterio, strings written
+# into datasets need to be converted from fixed to variable, otherwise
+# kealib will crash
+PYFILE=../geoh5/kea/rewrite_strings.py
+FILE=file-3.kea
+
+python $PYFILE --filename $FILE
+
+echo "4 CPU's"
+mpiexec -n 4 python parallel_kea_example.py
+PYFILE=../geoh5/kea/rewrite_strings.py
+FILE=file-3.kea
