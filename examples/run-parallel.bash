77e1081e6e1269d9be3efeff1961ee1965a2ce3d
Josh Sixsmith <josh.sixsmith@gmail.com>
2016-01-30 18:17:42 +1100

    Now calls rewrite_strings.py as an executeable.

--- examples/run-parallel.bash
+++ examples/run-parallel.bash
@@ -6,11 +6,9 @@ mpiexec -n 1 python parallel_kea_example.py
 # if using the KEA library directly or via GDAL, rasterio, strings written
 # into datasets need to be converted from fixed to variable, otherwise
 # kealib will crash
-PYFILE=../geoh5/kea/rewrite_strings.py
 FILE=file-parallel.kea
-
-python $PYFILE --filename $FILE
+rewrite_strings.py --filename $FILE
 
 echo "4 CPU's"
 mpiexec -n 4 python parallel_kea_example.py
-python $PYFILE --filename $FILE
+rewrite_strings.py --filename $FILE
